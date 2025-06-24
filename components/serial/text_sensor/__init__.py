144c3e6d02ada3e2d958e5bdc16f0ee64cb15cc2
bobr3940-2 <100007685+bobr3940-2@users.noreply.github.com>
2025-06-23 21:02:15 -0400

    Update __init__.py
    WARNING Using `text_sensor.TEXT_SENSOR_SCHEMA` is deprecated and will be removed in ESPHome 2025.11.0. Please use `text_sensor.text_sensor_schema(...)` instead. If you are seeing this, report an issue to the external_component author and ask them to update it.

--- components/serial/text_sensor/__init__.py
+++ components/serial/text_sensor/__init__.py
@@ -12,7 +12,7 @@ serial_ns = cg.esphome_ns.namespace('serial')
 SerialTextSensor = serial_ns.class_('SerialTextSensor', cg.Component,
                                     text_sensor.TextSensor, uart.UARTDevice)
 
-CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend({
+CONFIG_SCHEMA = text_sensor.text_sensor_schema.extend({
     cv.GenerateID(): cv.declare_id(SerialTextSensor),
 }).extend(uart.UART_DEVICE_SCHEMA)
 
